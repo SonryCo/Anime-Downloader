f21e4dbc0a47a2dd540e36988120b5580be1b99b
sonry <sonry@WIN-P51E657OQN6>
2016-09-20 14:33:27 -0300

    Agregar archivos de proyecto.

---
+++ Anime Downloader/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Anime_Downloader {
+    partial class Form1 {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing&&(components!=null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialSingleLineTextField1 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.animePic = new System.Windows.Forms.PictureBox();
+            this.CookieMonster1 = new System.Windows.Forms.WebBrowser();
+            this.lblTitulo = new MaterialSkin.Controls.MaterialLabel();
+            this.lblEpis = new MaterialSkin.Controls.MaterialLabel();
+            this.lblCont = new MaterialSkin.Controls.MaterialLabel();
+            this.t2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.materialTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.animePic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.AutoSize = true;
+            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton1.BackColor = System.Drawing.Color.White;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Location = new System.Drawing.Point(348, 85);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = false;
+            this.materialFlatButton1.Size = new System.Drawing.Size(91, 36);
+            this.materialFlatButton1.TabIndex = 0;
+            this.materialFlatButton1.Text = "Descargar";
+            this.materialFlatButton1.UseVisualStyleBackColor = false;
+            // 
+            // materialSingleLineTextField1
+            // 
+            this.materialSingleLineTextField1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.materialSingleLineTextField1.Depth = 0;
+            this.materialSingleLineTextField1.Hint = "";
+            this.materialSingleLineTextField1.Location = new System.Drawing.Point(7, 6);
+            this.materialSingleLineTextField1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSingleLineTextField1.Name = "materialSingleLineTextField1";
+            this.materialSingleLineTextField1.PasswordChar = '\0';
+            this.materialSingleLineTextField1.SelectedText = "";
+            this.materialSingleLineTextField1.SelectionLength = 0;
+            this.materialSingleLineTextField1.SelectionStart = 0;
+            this.materialSingleLineTextField1.Size = new System.Drawing.Size(494, 23);
+            this.materialSingleLineTextField1.TabIndex = 1;
+            this.materialSingleLineTextField1.Text = "http://Jkanime.net";
+            this.materialSingleLineTextField1.UseSystemPasswordChar = false;
+            // 
+            // materialTabControl1
+            // 
+            this.materialTabControl1.Controls.Add(this.tabPage1);
+            this.materialTabControl1.Controls.Add(this.tabPage2);
+            this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.Location = new System.Drawing.Point(0, 370);
+            this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(515, 189);
+            this.materialTabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Controls.Add(this.materialSingleLineTextField1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(507, 163);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Modo Normal";
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.materialFlatButton1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(507, 163);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Modo Lista";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // materialTabSelector1
+            // 
+            this.materialTabSelector1.BaseTabControl = this.materialTabControl1;
+            this.materialTabSelector1.Depth = 0;
+            this.materialTabSelector1.Location = new System.Drawing.Point(0, 64);
+            this.materialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabSelector1.Name = "materialTabSelector1";
+            this.materialTabSelector1.Size = new System.Drawing.Size(733, 21);
+            this.materialTabSelector1.TabIndex = 4;
+            this.materialTabSelector1.Text = "materialTabSelector1";
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(12, 98);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(131, 19);
+            this.materialLabel1.TabIndex = 5;
+            this.materialLabel1.Text = "Anime en Emision";
+            // 
+            // animePic
+            // 
+            this.animePic.Location = new System.Drawing.Point(0, 85);
+            this.animePic.Name = "animePic";
+            this.animePic.Size = new System.Drawing.Size(614, 279);
+            this.animePic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.animePic.TabIndex = 6;
+            this.animePic.TabStop = false;
+            this.animePic.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // CookieMonster1
+            // 
+            this.CookieMonster1.Location = new System.Drawing.Point(521, 370);
+            this.CookieMonster1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.CookieMonster1.Name = "CookieMonster1";
+            this.CookieMonster1.Size = new System.Drawing.Size(62, 54);
+            this.CookieMonster1.TabIndex = 7;
+            this.CookieMonster1.Visible = false;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo.Depth = 0;
+            this.lblTitulo.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(7, 213);
+            this.lblTitulo.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(88, 19);
+            this.lblTitulo.TabIndex = 8;
+            this.lblTitulo.Text = "Sonry\'S Life";
+            // 
+            // lblEpis
+            // 
+            this.lblEpis.AutoSize = true;
+            this.lblEpis.Depth = 0;
+            this.lblEpis.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lblEpis.ForeColor = System.Drawing.Color.White;
+            this.lblEpis.Location = new System.Drawing.Point(7, 332);
+            this.lblEpis.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblEpis.Name = "lblEpis";
+            this.lblEpis.Size = new System.Drawing.Size(88, 19);
+            this.lblEpis.TabIndex = 9;
+            this.lblEpis.Text = "Episodio 39";
+            // 
+            // lblCont
+            // 
+            this.lblCont.AutoSize = true;
+            this.lblCont.Depth = 0;
+            this.lblCont.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lblCont.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblCont.Location = new System.Drawing.Point(565, 98);
+            this.lblCont.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblCont.Name = "lblCont";
+            this.lblCont.Size = new System.Drawing.Size(39, 19);
+            this.lblCont.TabIndex = 10;
+            this.lblCont.Text = "1/10";
+            // 
+            // t2
+            // 
+            this.t2.Interval = 4000;
+            this.t2.Tick += new System.EventHandler(this.t2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(330, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(616, 539);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCont);
+            this.Controls.Add(this.lblEpis);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.CookieMonster1);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.materialTabSelector1);
+            this.Controls.Add(this.materialTabControl1);
+            this.Controls.Add(this.animePic);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AnimeDownloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.materialTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.animePic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField1;
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private MaterialSkin.Controls.MaterialTabSelector materialTabSelector1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private System.Windows.Forms.PictureBox animePic;
+        private System.Windows.Forms.WebBrowser CookieMonster1;
+        private MaterialSkin.Controls.MaterialLabel lblTitulo;
+        private MaterialSkin.Controls.MaterialLabel lblEpis;
+        private MaterialSkin.Controls.MaterialLabel lblCont;
+        private System.Windows.Forms.Timer t2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Anime Downloader/Form1.cs
@@ -0,0 +1,167 @@
+﻿using HtmlAgilityPack;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anime_Downloader {
+    public partial class Form1 : MaterialForm {
+        public Form1() {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme=MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme=new ColorScheme(Primary.Cyan800, Primary.Cyan900, Primary.Cyan500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        private WebClient WebCli = new WebClient();
+        private WebClient WebDl = new WebClient();
+        public ArrayList ln = new ArrayList();
+        public ArrayList tc = new ArrayList();
+        public ArrayList aim = new ArrayList();
+        public int cont = 0;
+        public int r = 0;
+        //SADOLDRES
+        private HtmlAgilityPack.HtmlDocument SNav(string url) {
+            WebCli.Encoding=Encoding.UTF8;
+            WebCli.Headers[HttpRequestHeader.UserAgent]="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
+            WebCli.Headers[HttpRequestHeader.Cookie]=CookieMonster1.Document.Cookie;
+            HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+            try {
+                string html = this.WebCli.DownloadString(url);
+                document.LoadHtml(html);
+                return document;
+            }
+            catch (WebException exception) {
+                if (exception.Message.Contains("404")) {
+                    //this.ilog=exception.Message;
+                }
+                else {
+                    MessageBox.Show("Error De Windows:"+exception.Message+"\n En caso de ser grave, infomar a Sonry", "No te preocupes");
+                }
+                return document;
+            }
+        }
+
+        //Descargas de Lista
+        private void BajarLista(Uri url, string path) {
+            int refe = 0;
+        //TIMER    autoDownloader.Enabled=false;
+            WebDl.Encoding=Encoding.UTF8;
+            WebDl.Headers[HttpRequestHeader.UserAgent]="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
+            WebDl.DownloadFileAsync(url, path);
+            WebDl.DownloadProgressChanged+=delegate (object s, DownloadProgressChangedEventArgs e) {
+       //         label10.Text="Descargando: "+listBox1.Items[0]+":";
+       //PB         progressBar1.Value=e.ProgressPercentage;
+       //         autoDownloader.Enabled=false;
+                if (e.ProgressPercentage>=0x5f) {
+                    refe=1;
+                }
+                else {
+                    refe=0;
+                }
+            };
+            WebDl.DownloadFileCompleted+=delegate (object s, AsyncCompletedEventArgs e) {
+            //PONER PBAR   progressBar1.Value=0;
+                if ((refe==0)&&(cont>0)) {
+            //Label de avisos        label10.Text="Detenido, Error x03";
+           //         delLis();
+                }
+                else {
+          //          label10.Text="Descargas Completadas:";
+                    refe=0;
+         //           timers=0;
+         //           timer1.Enabled=true;
+                }
+            };
+        }
+
+        //Destacados
+        private void obtenerDestacados(string fuente) {
+            HtmlAgilityPack.HtmlDocument document = SNav(fuente);
+            HtmlNodeCollection EtA = document.DocumentNode.SelectNodes("//a");
+            HtmlNodeCollection Img = document.DocumentNode.SelectNodes("//img");
+            HtmlNodeCollection EpS = document.DocumentNode.SelectNodes("//strong");
+
+            if (EtA!=null) {
+                foreach (HtmlNode Et in (IEnumerable<HtmlNode>)EtA) {
+                    HtmlAttribute attribute = Et.Attributes["href"];
+                    if (Et.InnerHtml.Contains("#htmlcaption-")) {
+                        ln.Add(attribute.Value);
+                    }
+                }
+            }
+            if (Img!=null) {
+                foreach (HtmlNode Im in (IEnumerable<HtmlNode>)Img) {
+                    HtmlAttribute attribute = Im.Attributes["src"];
+
+                   if (attribute.Value.Contains("jkvideo")) {
+                    //MessageBox.Show(attribute.Value);
+                    aim.Add(attribute.Value);
+
+                    }
+                }
+            }
+            if (EpS!=null) {
+                foreach (HtmlNode Ep in (IEnumerable<HtmlNode>)EpS) {
+                    if (Ep.InnerHtml.Contains("Episodio")) {
+                        string str = Ep.InnerHtml.Replace("\n", "").Replace(" ", "").Replace("Episodio", "Episodio ").Replace("\t", "");
+                        tc.Add(str);
+                    }
+                    else {
+                        tc.Add(Ep.InnerHtml.Replace("\n", ""));
+                    }
+                }
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e) {
+            CookieMonster1.Navigate("http://jkanime.net");
+            CookieMonster1.Stop();
+            label1.Parent=animePic;
+            //lblCont.Parent=animePic;
+            //lblEpis.Parent=animePic;
+            lblTitulo.Parent=animePic;
+            obtenerDestacados("http://jkanime.net");
+            t2.Enabled=true;
+            lblTitulo.Text=tc[r+r].ToString();
+            lblEpis.Text=tc[(r+r)+1].ToString();
+            animePic.ImageLocation = aim[r].ToString();
+            //            this.linkDest = this.ln[this.r].ToString();
+            lblCont.Text=(r+1)+"/"+ln.Count;
+            r++;
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void t2_Tick(object sender, EventArgs e) {
+            lblTitulo.Text=tc[r+r].ToString();
+            lblEpis.Text=tc[(r+r)+1].ToString();
+            animePic.ImageLocation=aim[r].ToString();
+            lblCont.Text=(r+1)+"/"+ln.Count;
+            //linkDest=this.ln[this.r].ToString();
+            if (((r+r)+1)<(tc.Count-1)) {
+                r++;
+            }
+            else {
+                r=0;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
